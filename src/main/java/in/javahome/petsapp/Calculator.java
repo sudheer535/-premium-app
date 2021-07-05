68dedf4f39ea2c0c4b121133bc515a1d80c95756
sudheer <nethisudheer535@gmail.com>
2021-07-06 00:04:04 +0530

    new

---
+++ src/main/java/in/javahome/petsapp/Calculator.java
@@ -0,0 +1,11 @@
+package in.javahome.petsapp;
+
+public class Calculator {
+	public int add(int i, int j) {
+		return i+j;
+	}
+	
+	public void someMethod() {
+		System.out.println("This is dummy method");
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1 style="color:red">Java Home App 3.0 - Weekend Batch!</h1>
+  
+  
+</body>
+</html>
